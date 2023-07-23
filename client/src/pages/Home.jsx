210da7dcdfa28e830d8aa1220fec617c00ac324b
Mayur Ramesh Mali <mayur1708mali@gmail.com>
2023-07-23 21:12:10 +0530

    Update url

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -25,12 +25,15 @@ const Home = () => {
       setLoading(true);
 
       try {
-        const response = await fetch('http://localhost:3000/api/v1/post', {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        });
+        const response = await fetch(
+          'https://dall-e-95cx.onrender.com/api/v1/post',
+          {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          }
+        );
 
         if (response.ok) {
           const result = await response.json();
